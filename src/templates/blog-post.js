cdc1ad180328c7c97daf35ab6a7829a749bf490f
An Tran <antran.inbox@gmail.com>
2018-08-26 17:35:38 +0700

    Add disqus comment to the post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,65 +4,77 @@ import Link from 'gatsby-link'
 import get from 'lodash/get'
 
 import Bio from '../components/Bio'
-import { rhythm, scale } from '../utils/typography'
+import {rhythm, scale} from '../utils/typography'
+
+import {DiscussionEmbed} from 'disqus-react';
 
 class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title')
-    const { previous, next } = this.props.pathContext
+    render() {
+        const post = this.props.data.markdownRemark
+        const siteTitle = get(this.props, 'data.site.siteMetadata.title')
+        const {previous, next} = this.props.pathContext;
 
-    return (
-      <div>
-        <Helmet title={`${post.frontmatter.title} | ${siteTitle}`} />
-        <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: 'block',
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio />
+        let siteMeta = get(this.props, 'data.site.siteMetadata');
 
-        <ul
-          style={{
-            display: 'flex',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            listStyle: 'none',
-            padding: 0,
-          }}
-        >
-          <li>
-            {
-              previous &&
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            }
-          </li>
-          <li>
-            {
-              next &&
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            }
-          </li>
-        </ul>
-      </div>
-    )
-  }
+        let disqusProps = {
+            shortname: siteMeta.disqus.shortName,
+            config: {
+                identifier: post.id, // identify of the post
+                title     : post.frontmatter.title, // title of the post
+            },
+        };
+
+        return (
+            <div>
+                <Helmet title={`${post.frontmatter.title} | ${siteTitle}`}/>
+                <h1>{post.frontmatter.title}</h1>
+                <p
+                    style={{
+                        ...scale(-1 / 5),
+                        display     : 'block',
+                        marginBottom: rhythm(1),
+                        marginTop   : rhythm(-1),
+                    }}
+                >
+                    {post.frontmatter.date}
+                </p>
+                <div dangerouslySetInnerHTML={{__html: post.html}}/>
+                <hr
+                    style={{
+                        marginBottom: rhythm(1),
+                    }}
+                />
+                <Bio/>
+                <ul
+                    style={{
+                        display       : 'flex',
+                        flexWrap      : 'wrap',
+                        justifyContent: 'space-between',
+                        listStyle     : 'none',
+                        padding       : 0,
+                    }}
+                >
+                    <li>
+                        {
+                            previous &&
+                            <Link to={previous.fields.slug} rel="prev">
+                                ← {previous.frontmatter.title}
+                            </Link>
+                        }
+                    </li>
+                    <li>
+                        {
+                            next &&
+                            <Link to={next.fields.slug} rel="next">
+                                {next.frontmatter.title} →
+                            </Link>
+                        }
+                    </li>
+                </ul>
+                <DiscussionEmbed {...disqusProps}/>
+            </div>
+        )
+    }
 }
 
 export default BlogPostTemplate
@@ -73,6 +85,9 @@ export const pageQuery = graphql`
       siteMetadata {
         title
         author
+        disqus {
+            shortName
+        }
       }
     }
     markdownRemark(fields: { slug: { eq: $slug } }) {
